3d51783eea11cab4596afb613277ecbb8c82990c
Oluwadabira <adigunsamuelaso@gmail.com>
2022-10-16 00:26:10 +0100

    finished version 2

--- script.js
+++ script.js
@@ -1,14 +1,70 @@
 const grid = document.querySelector('#Container');
-const clearButton = document.querySelector('#clearBtn')
-let value = prompt("enter ur value");
-parseInt(value);
-
+const clearButton = document.querySelector('#clearBtn');
+const rainbow = document.querySelector('#rainbow');
+const eraser = document.querySelector('#eraser');
+const slider = document.querySelector('#mySlider')
+const screenVal = document.querySelector('.value')
+const blackbtn = document.querySelector('#black');
 createGrid();
-clearButton.addEventListener('click', reloadGrid);
+
+
+slider.addEventListener('input', () => {
+    let val = document.getElementById('mySlider').value;
+    screenVal.textContent = `${val} x ${val}`;
+    removeNodes(grid);
+    grid.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${val}, 1fr)`;
+    for (let i = 1; i <= val * val; i++) {
+        const gridElement = document.createElement('div');
+        gridElement.classList.add('gridCell');
+        gridElement.addEventListener('mouseover', function (event) {
+                event.target.style.backgroundColor = `black`;
+        });
+        grid.appendChild(gridElement);
+    }
+});
+
+rainbow.addEventListener('click', function(){
+    let val = document.getElementById('mySlider').value;
+    let cell = grid.children;
+    for (let i = 1; i <= val * val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = getRandomColor();
+        }); 
+    }
+} );
+blackbtn.addEventListener('click', function(){
+    let val = document.getElementById('mySlider').value;
+    let cell = grid.children;
+    for (let i = 1; i <= val * val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = `black`;
+        }); 
+    }
+})
+eraser.addEventListener('click', function(){
+    let val = document.getElementById('mySlider').value;
+    let cell = grid.children;
+    for (let i = 1; i <= val * val; i++) {
+        cell[i].addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = `white`;
+        }); 
+    } 
+})
+clearButton.addEventListener('click', function(){
+    let val = document.getElementById('mySlider').value;
+    let cell = grid.children;
+    for (let i = 1; i <= val * val; i++) {
+        cell[i].style.backgroundColor = `white`
+    }
+}); 
+
+
 function createGrid(){
-    grid.style.gridTemplateColumns = `repeat(${value}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${value}, 1fr)`
-    for(let i = 1; i <= value * value; i++ ){
+    let val = document.getElementById('mySlider').value;
+    grid.style.gridTemplateColumns = `repeat(${val}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${val}, 1fr)`
+    for(let i = 1; i <= val * val; i++ ){
         const gridElement = document.createElement('div')
         gridElement.classList.add('gridCell')
         gridElement.addEventListener('mouseover', function(event){
@@ -17,10 +73,16 @@ function createGrid(){
         grid.appendChild(gridElement)
     }
 }
-function clearGrid(){
-   grid.innerHTML = ''
+function getRandomColor(){
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
 }
-function reloadGrid(){
-    clearGrid();
-    createGrid();
+function removeNodes(parent){
+    while(parent.firstChild){
+        parent.removeChild(parent.firstChild);
+    }
 }
